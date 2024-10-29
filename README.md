4f4f0abc0c4097704fc286270973aea56e3f1f60
Elry <mail@elry.moe>
2024-10-29 19:05:15 +0100

    Created the Plugin.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Unity-Editor-RPC
-Discord Rich Presence for the Editor, fully automatic.
+# UnityRPC
+
+UnityRPC is a minimal Unity plugin that displays the current project you are working on in Unity</br>
+Made this cuz all other Plugins that did this before, are unmaintained or abandanoned....
+
+## Features
+- Displays the Unity version name, project name and work duration in Discord Rich Presence.
+- Extremly Lightweight and has minimal performance impact.
+- Cross-platform compatibility.
+
+## Dependencies
+You need the [Discord SDK](https://github.com/thetestgame/upm-discord-game-sdk) installed.
+
+## Installation
+1. Open the Package manager in Unity.
+2. Click on the + in the top right and then add via Git.
+3. Paste in this link and click Add: `https://github.com/ElryGH/UnityRPC.git`
+
+## Usage
+Its fully automatic, it starts the second you open the Project.
+
+## Compatibility
+- **Unity Version:** Unity 2022, Unity 6 & newer.
+- **Platforms:** Windows, macOS, Linux.
+
+## Contributing
+You may open feature request issues, but PR-Requests are not welcome, as this is mainly made for myself!</br>
+Feel free to fork this if you want changes that i dont want.
+
+## License
+This project is licensed under the GNU License. See the [LICENSE](LICENSE) file for more details.
---
+++ UnityEditorRPC.cs
@@ -0,0 +1,124 @@
+﻿#if UNITY_EDITOR
+using System;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+using System.Threading.Tasks;
+using Discord;
+using Debug = UnityEngine.Debug;
+using System.Diagnostics;
+
+[InitializeOnLoad]
+public static class UnityEditorRPC
+{
+    private const string applicationId = "1300876381994877099";
+    private static Discord.Discord discord;
+
+    private static long startTimestamp;
+    private static bool playMode = false;
+
+    private string largeimage;
+
+    #region Initialization
+    static UnityEditorRPC()
+    {
+        DelayStart();
+    }
+    public static async void DelayStart(int delay = 1000)
+    {
+        await Task.Delay(delay);
+        Init();
+    }
+
+    public static void Init()
+    {
+
+        // Start Discord plugin
+        try
+        {
+            discord = new Discord.Discord(long.Parse(applicationId), (long)CreateFlags.Default);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.ToString());
+            return;
+        }
+        
+        string unityver = Application.unityVersion;
+
+        unityver = unityver.Substring(0, 4);
+
+        switch (unityver)
+        {
+            case "6000":
+                largeicon = "unity6";
+                break;
+            case "2023":
+            case "2022":
+            case "2021":
+            case "2020":
+                largeicon = "unitymodern";
+                break;
+            default:
+                largeicon = "unityold"
+                break;
+        }
+
+        // Get start timestamp
+        TimeSpan timeSpan = TimeSpan.FromMilliseconds(EditorAnalyticsSessionInfo.elapsedTime);
+        startTimestamp = DateTimeOffset.Now.Add(timeSpan).ToUnixTimeSeconds();
+
+        // Update activity
+        EditorApplication.update += Update;
+        EditorApplication.playModeStateChanged += PlayModeChanged;
+        UpdateActivity();
+    }
+    #endregion
+
+    #region Update
+    private static void Update()
+    {
+        if(discord != null) discord.RunCallbacks();
+    }
+
+    private static void PlayModeChanged (PlayModeStateChange state)
+    {
+        if (EditorApplication.isPlaying != playMode)
+        {
+            playMode = EditorApplication.isPlaying;
+
+            UpdateActivity();
+        }
+    }
+
+    public static void UpdateActivity()
+    {
+        if (discord == null)
+        {
+            Init();
+            return;
+        }
+
+        Activity activity = new Activity
+        {
+
+            State = EditorSceneManager.GetActiveScene().name + " scene",
+            Details = Application.productName,
+            Timestamps = { Start = startTimestamp },
+            Assets =
+                {
+                    LargeImage = largeicon,
+                    LargeText = "Unity " + Application.unityVersion
+                },
+        };
+
+        discord.GetActivityManager().UpdateActivity(activity, result =>
+        {
+            if (result != Result.Ok) Debug.LogError(result.ToString());
+        });
+
+    }
+    #endregion
+
+}
+#endif
